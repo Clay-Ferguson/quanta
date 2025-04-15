d304e3455912cf4edda021db7b568ebb08d5e8aa
Clay Ferguson <wclayf@gmail.com>
2025-04-15 15:03:18 -0500

    Making Markdown links clickable

--- src/components/MarkdownComp.tsx
+++ src/components/MarkdownComp.tsx
@@ -1,9 +1,24 @@
 import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
 interface MarkdownDisplayProps {
   markdownContent: string;
 }
 
 export default function Markdown({ markdownContent }: MarkdownDisplayProps) {
-    return <ReactMarkdown>{markdownContent}</ReactMarkdown>;
+    return (
+        <ReactMarkdown
+            rehypePlugins={[rehypeRaw]}
+            remarkPlugins={[remarkGfm]}
+            components={{
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                a: ({ node, ...props }) => (
+                    <a target="_blank" rel="noopener noreferrer" {...props} />
+                )
+            }}
+        >
+            {markdownContent}
+        </ReactMarkdown>
+    );
 }
