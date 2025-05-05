f831710da62e664e81930150603b6c3021bfc807
Clay Ferguson <wclayf@gmail.com>
2025-05-05 13:16:30 -0500

    moved database locations

--- run-prod.sh
+++ run-prod.sh
@@ -7,7 +7,7 @@ export QUANTA_CHAT_PORT="443"
 # website to be available at https://... and web socket at wss://...
 export QUANTA_CHAT_SECURE="y"
 
-export QUANTA_CHAT_DB_FILE_NAME="./db/quanta-chat.db"
+export QUANTA_CHAT_DB_FILE_NAME="/home/clay/db/quanta-chat.db"
 export QUANTA_CHAT_CERT_PATH="/etc/letsencrypt/live/chat.quanta.wiki"
 export QUANTA_CHAT_ADMIN_PUBLIC_KEY="0357b752ea2b1bcc0365efa73ab0d573f1c27a948aa256394f991c8c09d8edb7df"
 
