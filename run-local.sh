8a41331b48c7213118928b47635b8b7d765d8f9c
Clay Ferguson <wclayf@gmail.com>
2025-05-07 12:37:14 -0500

    removing any tool-generated package-lock.json

--- run-local.sh
+++ run-local.sh
@@ -6,6 +6,8 @@ export QUANTA_CHAT_SECURE="n"
 export QUANTA_CHAT_DB_FILE_NAME="/home/clay/ferguson/chat-db/quanta-chat.db"
 export QUANTA_CHAT_ADMIN_PUBLIC_KEY="0357b752ea2b1bcc0365efa73ab0d573f1c27a948aa256394f991c8c09d8edb7df"
 
+# remove package-lock.json if anythiong ever creates it. This app uses Yarn instead.
+rm package-lock.json
 rm -rf ./dist
 QUANTA_DEV=true yarn build
 yarn start
