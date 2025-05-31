47e06272f83f0fea7d6ee75ee15014ae7a3e5541
Clay Ferguson <wclayf@gmail.com>
2025-05-30 20:40:31 -0500

    removed obsolete comment

--- run-dev.sh
+++ run-dev.sh
@@ -3,7 +3,6 @@
 export CONFIG_FILE="./config-dev.yaml"
 
 # remove package-lock.json if anythiong ever creates it. This app uses Yarn instead.
-# todo-0: need to separate these three lines into 'build-dev.sh' so we can run it from our deployment script
 rm -f package-lock.json
 rm -rf ./dist
 QUANTA_DEV=true yarn build
