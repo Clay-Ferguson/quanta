5978b368593b226f7d3dde3be4acc9b712c3be65
Clay Ferguson <wclayf@gmail.com>
2025-10-04 11:24:50 -0500

    removed obsolete grafana scripts

--- build/local/run-hidden.sh
+++ build/local/run-hidden.sh
@@ -4,9 +4,6 @@
 #       the same as run.sh except for this one running in a hidden terminal which is useful for the `QDash.desktop`
 #       way of running the app on Linux.
 
-# Configuration
-RUN_GRAFANA=${RUN_GRAFANA:-false}
-
 # Check if we're in the project root by looking for package.json
 if [ ! -f "./package.json" ]; then
     echo "Error: package.json not found. Run this script from the project root."
@@ -17,16 +14,6 @@ export CONFIG_FILE="./build/local/config.yaml"
 
 ./build/kill.sh
 rm -f quanta.log 
-
-# Run /dist/grafana/alloy/start.sh if it exists
-if [ "$RUN_GRAFANA" = "true" ]; then
-    if [ -f "./dist/grafana/alloy/restart.sh" ]; then
-        echo "Starting existing Grafana Alloy stack..."
-        bash ./dist/grafana/alloy/restart.sh
-    else
-        echo "No existing Grafana Alloy stack start script found. Continuing..."
-    fi
-fi
 
 # Start the Node.js app in a new session (completely detached)
 echo "Starting new AppServer.js process..."
