1ba52d15b0cf20d7cb0eeed5888069909c93b52b
Clay Ferguson <wclayf@gmail.com>
2025-10-23 20:18:10 -0500

    adding HTTP type safety

--- common/types/EndpointTypes.ts
+++ common/types/EndpointTypes.ts
@@ -6,6 +6,12 @@ export interface AuthenticationInfo {
     validSignature?: boolean; // Indicates if the request has a valid signature
 }
 
+// Base response interface that all API response types should extend
+export interface BaseResponse {
+    message?: string; // Success message
+    error?: string; // Error message if operation failed
+}
+
 export type DeleteRoom_Request = {
     roomName: string;
 }
@@ -86,9 +92,7 @@ export type Delete_ReqInfo = {
 }
 
 // HTTP Response info
-export type Delete_ResInfo = {
-    message?: string; // Success message
-    error?: string; // Single error message (for validation errors)
+export type Delete_ResInfo = BaseResponse & {
     deletedCount?: number; // Present in multiple items mode
     errors?: string[]; // Present if there were errors in multiple items mode
 }
@@ -101,10 +105,59 @@ export type MoveUpDown_ReqInfo = {
 }
 
 // HTTP Response info for move up/down operation
-export type MoveUpDown_ResInfo = {
-    message?: string; // Success message
-    error?: string; // Error message if operation failed
+export type MoveUpDown_ResInfo = BaseResponse & {
     file1?: string; // First file involved in the swap
     file2?: string; // Second file involved in the swap
+}
+
+// HTTP Request info for set public operation
+export type SetPublic_ReqInfo = {
+    is_public: boolean; // Target public accessibility status (true=public, false=private)
+    filename: string; // Name of the file or folder to modify
+    treeFolder: string; // Relative path to the parent directory
+    recursive?: boolean; // Optional flag to apply changes recursively to all nested content
+}
+
+// HTTP Response info for set public operation
+export type SetPublic_ResInfo = BaseResponse & {
+    // No additional properties beyond base response
+}
+
+// HTTP Request info for save file operation
+export type SaveFile_ReqInfo = {
+    filename: string; // Name of the file to save
+    content: string; // File content to save
+    treeFolder: string; // Relative path to the parent directory
+    newFileName?: string; // Optional new name for the file (triggers rename)
+    split?: boolean; // Whether to split content on '\n~\n' delimiter
+}
+
+// HTTP Response info for save file operation
+export type SaveFile_ResInfo = BaseResponse & {
+    // No additional properties beyond base response
+}
+
+// HTTP Request info for create file operation
+export type CreateFile_ReqInfo = {
+    fileName: string; // Name of the file to create
+    treeFolder: string; // Relative path to the parent directory
+    insertAfterNode: string; // Name of the file/folder after which to insert the new file
+}
+
+// HTTP Response info for create file operation
+export type CreateFile_ResInfo = BaseResponse & {
+    fileName?: string; // Name of the created file (may differ from requested name if collision occurred)
+}
+
+// HTTP Request info for create folder operation
+export type CreateFolder_ReqInfo = {
+    folderName: string; // Name of the folder to create
+    treeFolder: string; // Relative path to the parent directory
+    insertAfterNode: string; // Name of the file/folder after which to insert the new folder
+}
+
+// HTTP Response info for create folder operation
+export type CreateFolder_ResInfo = BaseResponse & {
+    folderName?: string; // Name of the created folder
 }
 
--- server/HttpTypes.ts
+++ server/HttpTypes.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { AuthenticationInfo, Delete_ReqInfo, Delete_ResInfo, MoveUpDown_ReqInfo, MoveUpDown_ResInfo } from "../common/types/EndpointTypes.js";
+import { AuthenticationInfo, Delete_ReqInfo, Delete_ResInfo, MoveUpDown_ReqInfo, MoveUpDown_ResInfo, SetPublic_ReqInfo, SetPublic_ResInfo, SaveFile_ReqInfo, SaveFile_ResInfo, CreateFile_ReqInfo, CreateFile_ResInfo, CreateFolder_ReqInfo, CreateFolder_ResInfo } from "../common/types/EndpointTypes.js";
 
 export type AuthenticatedRequest = Request & AuthenticationInfo; 
 
@@ -8,3 +8,15 @@ export type DeleteResponse = Response<Delete_ResInfo>;
 
 export type MoveUpDownRequest = Request<any, any, MoveUpDown_ReqInfo> & AuthenticationInfo;
 export type MoveUpDownResponse = Response<MoveUpDown_ResInfo>;
+
+export type SetPublicRequest = Request<any, any, SetPublic_ReqInfo> & AuthenticationInfo;
+export type SetPublicResponse = Response<SetPublic_ResInfo>;
+
+export type SaveFileRequest = Request<any, any, SaveFile_ReqInfo> & AuthenticationInfo;
+export type SaveFileResponse = Response<SaveFile_ResInfo>;
+
+export type CreateFileRequest = Request<any, any, CreateFile_ReqInfo> & AuthenticationInfo;
+export type CreateFileResponse = Response<CreateFile_ResInfo>;
+
+export type CreateFolderRequest = Request<any, any, CreateFolder_ReqInfo> & AuthenticationInfo;
+export type CreateFolderResponse = Response<CreateFolder_ResInfo>;
