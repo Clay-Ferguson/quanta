22cde73b3a19cf555d866781f7c6824d63a0c762
Clay Ferguson <wclayf@gmail.com>
2025-10-26 15:44:21 -0500

    finished separating http types for plugins

--- common/types/EndpointTypes.ts
+++ common/types/EndpointTypes.ts
@@ -1,8 +1,4 @@
-import { AttachmentInfo, ChatMessageIntf, RoomInfo, UserProfileCompact } from "./CommonTypes.js";
-
-
-// todo-0: These are incomplete, not consistently used throughout the codebase, and even once done we need to split
-// apart to where each plugin has it's own copy of these types rather than having them all in common/types
+import { UserProfileCompact } from "./CommonTypes.js";
 
 export interface AuthenticationInfo {
     userProfile?: UserProfileCompact;
@@ -14,37 +10,5 @@ export interface BaseResponse {
     error?: string;
 }
 
-// &&&
 
-export type GetRoomInfo_ReqInfo = Record<string, never>;
-
-export type GetRoomInfo_ResInfo = BaseResponse & {
-    rooms: RoomInfo[];
-}
-
-export type DeleteRoom_ReqInfo = {
-    roomName: string;
-}
-
-export type DeleteRoom_ResInfo = BaseResponse & {
-    message?: string;
-}
-
-export type GetRecentAttachments_ReqInfo = Record<string, never>;
-
-export type GetRecentAttachments_ResInfo = {
-    attachments: AttachmentInfo[]
-}
-
-export type GetMessagesByIds_Response = {
-    messages: ChatMessageIntf[];
-}
-
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export type DeleteAttachment_ReqInfo = {
-}
-
-export type DeleteAttachment_ResInfo = BaseResponse & {
-}
 
--- server/HttpTypes.ts
+++ server/HttpTypes.ts
@@ -1,6 +1,5 @@
-import { Request, Response } from 'express';
-import { AuthenticationInfo, GetRoomInfo_ReqInfo, GetRoomInfo_ResInfo, DeleteRoom_ReqInfo, DeleteRoom_ResInfo, GetRecentAttachments_ReqInfo, GetRecentAttachments_ResInfo, DeleteAttachment_ReqInfo, DeleteAttachment_ResInfo } from "../common/types/EndpointTypes.js";
-import { GetMessageIdsForRoom_ReqInfo, GetMessageIdsForRoom_ResInfo } from '../plugins/chat/client/EndpointTypes.js';
+import { Request } from 'express';
+import { AuthenticationInfo } from "../common/types/EndpointTypes.js";
 
 // todo-0: These are incomplete, not consistently used throughout the codebase, and even once done we need to split
 // apart to where each plugin has it's own copy of these types rather than having them all in common/types
@@ -8,17 +7,4 @@ import { GetMessageIdsForRoom_ReqInfo, GetMessageIdsForRoom_ResInfo } from '../p
 export type AuthenticatedRequest = Request & AuthenticationInfo; 
 
 
-export type GetMessageIdsForRoomRequest = Request<{ roomId: string }, any, any, GetMessageIdsForRoom_ReqInfo>;
-export type GetMessageIdsForRoomResponse = Response<GetMessageIdsForRoom_ResInfo>;
 
-export type GetRoomInfoRequest = Request<any, any, GetRoomInfo_ReqInfo> & AuthenticationInfo;
-export type GetRoomInfoResponse = Response<GetRoomInfo_ResInfo>;
-
-export type DeleteRoomRequest = Request<any, any, DeleteRoom_ReqInfo> & AuthenticationInfo;
-export type DeleteRoomResponse = Response<DeleteRoom_ResInfo>;
-
-export type GetRecentAttachmentsRequest = Request<any, any, GetRecentAttachments_ReqInfo> & AuthenticationInfo;
-export type GetRecentAttachmentsResponse = Response<GetRecentAttachments_ResInfo>;
-
-export type DeleteAttachmentRequest = Request<any, any, DeleteAttachment_ReqInfo> & AuthenticationInfo;
-export type DeleteAttachmentResponse = Response<DeleteAttachment_ResInfo>;
